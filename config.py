4a5f258f5c1183bfd7913348bf7e0bef9b2f1d4a
Mike Kelly <mikekelly321@gmail.com>
2024-08-13 13:23:10 +0100

    make default model configurable - closes #2

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
 
-if os.environ["OPENAI_API_KEY"]:
-    from langchain_openai import ChatOpenAI
-    default_langchain_model = ChatOpenAI(model="gpt-4o", temperature=0)
+default_model_temperature = int(os.getenv("DEFAULT_MODEL_TEMPERATURE", "0"))
+default_model_provider = os.getenv("DEFAULT_MODEL_PROVIDER", "OPENAI").upper()
+default_model_name = os.getenv("DEFAULT_MODEL_NAME", "gpt-4o")
+
+match default_model_provider:
+    case "OPENAI":
+        default_langchain_model = ChatOpenAI(model_name=default_model_name, temperature=default_model_temperature)
+    case "ANTHROPIC":
+        default_langchain_model = ChatAnthropic(model_name=default_model_name, temperature=default_model_temperature)
